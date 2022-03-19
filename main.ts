29f1ed7c0d7a2c3e95ae0771482d784333dde682
DanielY121 <96324127+DanielY121@users.noreply.github.com>
2022-03-19 20:26:51 +0800

    (haven’t done yet)

--- main.ts
+++ main.ts
@@ -8,15 +8,15 @@ input.onButtonPressed(Button.AB, function () {
             Enemy.delete()
             Bullet.delete()
             game.addScore(1)
-        } else if (false) {
+        } else if (Enemy_2.isTouching(null)) {
         	
-        } else if (false) {
+        } else if (Enemy_3.isTouching(null)) {
         	
-        } else if (false) {
+        } else if (Enemy_4.isTouching(null)) {
         	
-        } else if (false) {
+        } else if (Enemy_5.isTouching(null)) {
         	
-        } else if (false) {
+        } else if (Enemy_6.isTouching(null)) {
         	
         } else {
         	
@@ -27,6 +27,11 @@ input.onButtonPressed(Button.B, function () {
     Player.change(LedSpriteProperty.X, 1)
 })
 let Bullet: game.LedSprite = null
+let Enemy_6: game.LedSprite = null
+let Enemy_5: game.LedSprite = null
+let Enemy_4: game.LedSprite = null
+let Enemy_3: game.LedSprite = null
+let Enemy_2: game.LedSprite = null
 let Enemy: game.LedSprite = null
 let Player: game.LedSprite = null
 Player = game.createSprite(2, 4)
@@ -41,6 +46,11 @@ if (Random_enemy_value == 1) {
     basic.showString("Enemy fever ")
     basic.pause(3000)
     Enemy = game.createSprite(2, 2)
+    Enemy_2 = game.createSprite(2, 2)
+    Enemy_3 = game.createSprite(2, 2)
+    Enemy_4 = game.createSprite(2, 2)
+    Enemy_5 = game.createSprite(2, 2)
+    Enemy_6 = game.createSprite(2, 2)
 }
 basic.forever(function () {
 	
